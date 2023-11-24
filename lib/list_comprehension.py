ddecd185c11ef7d9e1256c309be4a6c7c0fd816d
root <root@DESKTOP-UEO62DE.localdomain>
2023-11-24 11:18:06 +0300

    Pass comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+   
+    if num_list == []:
+       return []
+    else:
+        num_list = [num for num in num_list if num % 2 == 0]
+    return num_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list == []:
+        return []
+    else:
+        sentence_list = [sentence + '!' for sentence in sentence_list]
+        return sentence_list
+ 
+
